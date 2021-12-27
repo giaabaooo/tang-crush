a7b70dc4ffccb1e51d09ac648cc98bf8038d01c9
giaabaooo <96742694+giaabaooo@users.noreply.github.com>
2021-12-28 00:51:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Yvan à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Làm ny anh nhé ?',
+    btnNo: 'Không, chúng ta chỉ là bạn thôi !',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại thích em mất rồi ',
+    btnReply: 'Send to baobadboi ',
+    reply: 'Yêu thì yêu mà không yêu thì phải yêu ',
+    mess: 'Anh biết mà.Yêu Yvan nhiều nhiều',
+    messDesc: 'Tối nay 7h anh qua đón nhé cô bé thái nguyên',
+    btnAccept: 'vâng yêu anh ',
+    messLink: 'http://fb.com' //https://www.facebook.com/ng.gia.baoooo
 }
